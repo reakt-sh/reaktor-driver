46091f18dc8b49da699ef644610612a2b980d674
Alexander Schulz-Rosengarten <als@informatik.uni-kiel.de>
2026-06-02 16:42:43 +0200

    Configured automatic breaking if speed exceed target speed to be basically disabled

--- connector/connector/generated/config_driver.py
+++ connector/connector/generated/config_driver.py
@@ -4,4 +4,4 @@
 # Configuration parameters for the driver behavior
 
 DRIVER_REVOLUTION_SIGNAL_BLIND_TIME = 2 # Time in millis where interrupts are ignored because the signal might be fuzzy and result in multiple signal edges.
-DRIVER_BRAKING_THRESHOLD_RPM = 80 # Threshold as difference between current and target speed (in RPM) at which the brakes will be engaged to further decelerate.
+DRIVER_BRAKING_THRESHOLD_RPM = 1000 # Threshold as difference between current and target speed (in RPM) at which the brakes will be engaged to further decelerate.
