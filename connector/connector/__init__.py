9540956db634cf287e0b8a2601237f2df261b4bc
Alexander Schulz-Rosengarten <als@informatik.uni-kiel.de>
2026-09-01 14:07:56 +0200

    connector: Added new Exception to package

--- connector/connector/__init__.py
+++ connector/connector/__init__.py
@@ -6,6 +6,6 @@ import logging
 CONNECTOR_ROOT_LOGGER = logging.getLogger("connector")
 
 from .connection import Connection
-from .data import Control, Status, Mode, ErrorState, InternalState
+from .data import Control, Status, Mode, ErrorState, InternalState, ConnectionProblem
 
-__all__ = ["Connection", "Control", "Status", "Mode", "ErrorState", "InternalState"]
+__all__ = ["Connection", "Control", "Status", "Mode", "ErrorState", "InternalState", "ConnectionProblem"]
